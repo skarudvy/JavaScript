8fee7022939b0b520d2840155edc983601fa865c
gGpNam <gpnam@gtone.co.kr>
2017-11-10 01:24:05 +0900

    [imp][0001]draw_circle_onMouseDown git commit -m [imp][0001]draw_circle_onMouseDown git commit -m [imp][0001]draw_circle_onMouseDow

---
+++ Chapter1/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    'use strict';
+
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    
+    var tool = new Tool();
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'green';
+    }
+
+    paper.view.draw();
+
+    console.log('main.js loaded');
+})
